86b6696dd6b2b02b1f63c36770e43a2be29221c2
Squeshly-Freezed <251692768+Squeshly-Freezed@users.noreply.github.com>
2026-02-26 21:03:01 +1300

    Finish majority styling, add :hover button effects

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const myLibrary = [];
 
+const addBookButton = document.querySelector(".addbookbutton");
+
+
 function Book(title, author, pages, readStatus) {
     if (!new.target) {
         throw Error("You must use 'new' keyword when instantializing object");
@@ -10,3 +13,23 @@ function Book(title, author, pages, readStatus) {
     this.readStatus = readStatus;
     this.id = crypto.randomUUID();
 }
+
+Book.prototype.changeReadStatus = function() {
+    if (this.readStatus) {
+        !this.readStatus; 
+    } else if (!this.readStatus) {
+        this.readStatus;
+    }
+};
+
+function addBookToLibrary(title, author, pages, readStatus) {
+    const book = new Book("poo", "wee", 123, true);
+    myLibrary.push(book);
+    console.log(myLibrary);
+}
+
+function displayBooks() {
+    myLibrary.forEach((book, index) => {
+        //update DOM
+    });
+};
