b00a2993d033b1d0e62ec87212a365e3077cb733
Squeshly-Freezed <251692768+Squeshly-Freezed@users.noreply.github.com>
2026-02-26 16:55:05 +1300

    Add basic layout, book constructor

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(title, author, pages, readStatus) {
+    if (!new.target) {
+        throw Error("You must use 'new' keyword when instantializing object");
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+    this.id = crypto.randomUUID();
+}
